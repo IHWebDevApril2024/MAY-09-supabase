65628ab3387a52445a790baac494661b9954f9da
Marshall-Bits <kz_@hotmail.es>
2024-05-10 09:41:56 +0200

    add search query

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import supabase from "./supabase/config";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [query, setQuery] = useState("");
 
   const getUsers = async () => {
     const { data, error } = await supabase.from("users").select();
@@ -20,15 +21,45 @@ function App() {
     }
   };
 
+  const getSearch = async () => {
+    const { data, error } = await supabase
+      .from("users")
+      .select()
+      .ilike("user_name","%"+ query + "%");
+    if (error) {
+      console.log("There was an error: ", error);
+      return;
+    } else {
+      console.log("Query fetched successfully: ", data);
+      setUsers(data);
+    }
+  };
+
+  const handleSearch = (e) => {
+    const searchWord = e.target.value;
+    setQuery(searchWord);
+  };
+
   useEffect(() => {
     getUsers();
   }, []);
+
+  useEffect(() => {
+    if (query.length === 0) {
+      getUsers();
+      return;
+    }
+    getSearch();
+  }, [query]);
 
   return (
     <>
       <Link to="/">Home</Link>
       <span> || </span>
       <Link to="/create">New user</Link>
+      <div>
+        <input onChange={handleSearch} type="text" /> <span>🔍</span>
+      </div>
       <Routes>
         <Route path="/" element={<HomePage users={users} />} />
         <Route path="/create" element={<FormPage />} />
